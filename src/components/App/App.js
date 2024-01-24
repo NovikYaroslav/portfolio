4fd0ef32bd6fec4188bbdf1942aa1cfa20d84471
Novik <yaroslavnowik@yandex.ru>
2024-01-24 18:03:03 +0100

    ico and tittle changed, app moved, card component renamed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
-import Header from './components/Header/Header';
-import MyProjects from './pages/My-Projects';
-import Footer from './components/Footer/Footer';
+import Header from '../Header/Header';
+import MyProjects from '../../pages/My-Projects';
+import Footer from '../Footer/Footer';
 
 const darkTheme = createTheme({
   palette: {
